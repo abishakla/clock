822924dc2faca1032df99f655dc290b1668c6642
Abisha K <168981676+abishakla@users.noreply.github.com>
2024-12-21 14:18:01 +0530

    Add files via upload

---
+++ Qr-code-generator-main/qr code generator/script.js
@@ -0,0 +1,8 @@
+let image= document.getElementById("image")
+let qrimage= document.getElementById("qrimage")
+let qrtext= document.getElementById("qrtext")
+function generateqr()
+{
+  qrimage.src=" https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+qrtext.value;
+  }
+   
